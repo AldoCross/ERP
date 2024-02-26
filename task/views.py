3a0552c887073824bf1209657bdbe21be368b835
Aldo Cruz Sosa <112133794+AldoCross@users.noreply.github.com>
2024-02-25 17:01:36 -0800

    Segundo commit del proyecto
    se agrega el menú para viajar entre ventanas en la pagina web, se agrega validación de usuarios en la base de datos, se corrijen errores de programacion y se añaden nuevos archivos html.

--- task/views.py
+++ task/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
 from django.http import HttpResponse
+from django.db import IntegrityError
 
 # Create your views here.
 def home(request):
@@ -15,16 +17,30 @@ def signup(request):
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                User= User.objects.create_user(username=request.POST['username'],
-                password=request.POST['password1'])
-                User.save()
-                return HttpResponse('User created Successfully')
-            except:
-                return render(request, 'signup.html',{
-                    'form': UserCreationForm,
-                    "error": 'User already Exists'
-                })
+                user = User.objects.create_user(username=request.POST['username'],password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+               return render(request,'signup.html',{
+                'form': UserCreationForm,
+                "error": 'User already Exist'
+               })
+            
+        return render(request, 'signup.html',{
+                'form': UserCreationForm,
+                "error": 'User already Exists'
+        })
+        
         return render(request, 'signup.html',{
             'form': UserCreationForm,
             "error": 'Password do not match'
         })
+        
+def tasks(request):
+    return render(request,'tasks.html') 
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
